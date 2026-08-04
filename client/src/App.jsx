0b8e76c80f34a63761d4cb993f35713f63edf882
Simalbutt <simalbutt04@gmail.com>
2026-08-04 09:46:18 +0500

    added login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import { LandingPage } from './pages/LandingPage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Navbar} from './components/Navbar';
+import {LandingPage} from './pages/LandingPage';
+import {LoginPage} from './pages/LoginPage';
 
 export function AppRoutes() {
-  return (
-      <div className="min-h-screen bg-[#FAF9FA] text-slate-800 flex flex-col font-sans">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </div>
-  );
+    return (
+        <div className="min-h-screen bg-[#FAF9FA] text-slate-800 flex flex-col font-sans">
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<LandingPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default function App() {
-  return (
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppRoutes/>
+        </BrowserRouter>
+    );
 }
